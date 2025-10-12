cf833b28f04487d03caea68fef0a1aaaf37e5f88
Owen Jones <owen.d.jones@btinternet.com>
2025-10-12 15:48:18 +0100

    reformat how run is called to make more sense as a library

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! You can install this crate to use it as a binary or add it to a project to use it as a library
+//! * As a library it can run the cargo test command and parse the output into a list of ParsedTestGroups
+//! * As a binary it can be used with `cargo ptest`, see the readme for more information about using it as a binary
+
 pub mod config;
 pub mod display;
 pub mod parse;
